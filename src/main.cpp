dc71e9ee44a0e647e61035eed1fc27bd1bd820c5
Rajan Maghera <rmaghera@ualberta.ca>
2022-10-14 02:59:52 -0600

    Update program to run

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,57 @@
-#include "GeneratorLexer.h"
-#include "GeneratorParser.h"
+#include "RISCLexer.h"
+#include "RISCParser.h"
 
 #include "ANTLRFileStream.h"
 #include "CommonTokenStream.h"
 #include "tree/ParseTree.h"
 #include "tree/ParseTreeWalker.h"
+#include "ASTBuilder.h"
+#include "AST.h"
+#include "LabelReturn.h"
 
 #include <iostream>
+#include <string>
 #include <fstream>
+
+using namespace std;
 
 int main(int argc, char **argv) {
   if (argc < 3) {
     std::cout << "Missing required argument.\n"
-              << "Required arguments: <input file path> <output file path>\n";
+              << "Required arguments: <input file path> <function name>\n";
     return 1;
   }
 
-  // Open the file then parse and lex it.
+   string titleText =
+   "\n"
+   "  _______ ___ _______ _______ __             __    __       \n"
+   " |   _   |   |   _   |   _   |  |--.--.--.--|  .--|  .--.--.\n"
+   " |.  l   |.  |   1___|.  1___|  _  |  |  |  _  |  _  |  |  |\n"
+   " |.  _   |.  |____   |.  |___|_____|_____|_____|_____|___  |\n"
+   " |:  |   |:  |:  1   |:  1   |                       |_____|\n"
+   " |::.|:. |::.|::.. . |::.. . |                              \n"
+   " `--- ---`---`-------`-------'                              \n"
+   "                                                            ";
+
+    cout << titleText << endl;
+    cout << "Welcome to RISCbuddy! I'm your little friend that will help catch bugs in your RISC-V assembly code." << endl;
+    cout << "I'm going to run a few checks on your code to make sure it's correct. Hang tight and read the messages carefully." << endl;
+    cout << endl;
+
   antlr4::ANTLRFileStream afs;
   afs.loadFromFile(argv[1]);
-  generator::GeneratorLexer lexer(&afs);
+  risc::RISCLexer lexer(&afs);
   antlr4::CommonTokenStream tokens(&lexer);
-  generator::GeneratorParser parser(&tokens);
+  risc::RISCParser parser(&tokens);
 
-  // Get the root of the parse tree. Use your base rule name.
   antlr4::tree::ParseTree *tree = parser.file();
 
-  // HOW TO USE A LISTENER
-  // Make the listener
-  // MyListener listener;
-  // Walk the tree
-  // antlr4::tree::ParseTreeWalker::DEFAULT.walk(&listener, tree);
+  ASTBuilder astBuilder;
+  auto ast = any_cast<shared_ptr<AST>>(astBuilder.visit(tree));
 
-  // HOW TO USE A VISITOR
-  // Make the visitor
-  // MyVisitor visitor;
-  // Visit the tree
-  // visitor.visit(tree);
-
-  // HOW TO WRITE OUT.
-  // std::ofstream out(argv[2]);
-  // out << "This is out...\n";
+  auto* pass = new LabelReturn(argv[2]);
+  pass->run(ast);
 
   return 0;
 }
+
